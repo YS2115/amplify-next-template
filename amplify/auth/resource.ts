28f6753e87a6872b404538c84889fd8da3583810
is1e10 <aisato310ai@gmail.com>
2024-12-09 11:04:33 +0900

    google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,14 +3,16 @@ import { defineAuth, secret } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      loginWithAmazon: {
+        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+        scopes: ['email']
       },
       callbackUrls: [
-        'http://localhost:3000/profile'
+        'http://localhost:3000/profile',
+        'https://mywebsite.com/profile'
       ],
-      logoutUrls: ['http://localhost:3000/'],
+      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
     }
   }
 });
